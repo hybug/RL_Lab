dc7e4b85c2e63c991a426d0428be4964bf52e11c
hybug <1281261611@qq.com>
2021-01-11 21:16:43 +0800

    Update "distributed PPO algorithm"

---
+++ algorithm/dPPOc.py
@@ -0,0 +1,25 @@
+'''
+Author: hanyu
+Date: 2021-01-11 13:10:43
+LastEditTime: 2021-01-11 13:16:07
+LastEditors: hanyu
+Description: distributed PPO clip algorithm
+FilePath: /test_ppo/algorithm/dPPOc.py
+'''
+
+import tensorflow as tf
+from module import IS_from_logits
+
+
+def dPPOc(action, policy_logits, behavior_logits, advantage, clip):
+    ratio = IS_from_logits(
+        policy_logits=policy_logits,
+        action=action,
+        behavior_logits=behavior_logits)
+
+    neg_loss = advantage * ratio
+    if clip is not None:
+        ratio_clip = tf.clip_by_value(ratio, 1.0 - clip, 1.0 + clip)
+        neg_loss = tf.minimum(neg_loss, advantage * ratio_clip)
+    loss = -neg_loss
+    return loss
