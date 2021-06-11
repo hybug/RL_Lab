170a7d73ede90d0bd28de57a18cd40ff2f1baae3
hybug <1281261611@qq.com>
2021-06-11 18:44:21 +0800

    Update “Complete the step functino”

--- examples/PPO_hungry_geese/test/test_env.py
+++ examples/PPO_hungry_geese/test/test_env.py
@@ -1,13 +1,20 @@
 '''
 Author: hanyu
 Date: 2021-06-09 09:23:32
-LastEditTime: 2021-06-09 09:32:14
+LastEditTime: 2021-06-11 08:13:24
 LastEditors: hanyu
 Description: test env
 FilePath: /test_ppo/examples/PPO_hungry_geese/test/test_env.py
 '''
 
 from examples.PPO_hungry_geese.env import _warp_env
+import random
 
 Env = _warp_env()
 env = Env()
+
+obs = env.reset(True)
+is_terminal = False
+while not is_terminal:
+    actions = {idx: random.choice([0, 1, 2, 3]) for idx in range(env.num_agents)}
+    obs, reward, is_terminal, info = env.step(actions)
--- test/test.py
+++ test/test.py
@@ -1,27 +1,8 @@
 '''
 Author: hanyu
 Date: 2021-01-07 09:46:18
-LastEditTime: 2021-01-07 09:49:15
+LastEditTime: 2021-06-10 01:53:04
 LastEditors: hanyu
 Description: test
 FilePath: /test_ppo/test/test.py
 '''
-
-def wrapClass(cls):
-    def inner(a):
-        print('class name:', cls.__name__)
-        return cls(a)
-    return inner
-
-@wrapClass
-class Foo():
-    def __init__(self, a):
-        self.a = a
-
-    def fun(self):
-        print('self.a =', self.a)
-
-
-m = Foo('hello')
-m = wrapClass(Foo('hello'))
-# m.fun()
