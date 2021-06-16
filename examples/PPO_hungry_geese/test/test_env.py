c269603e59ffae0e421ca4b402248ef1a43f6533
hybug <1281261611@qq.com>
2021-06-16 17:11:13 +0800

    Update "Implement of Reward Function & Segments Saving"

--- examples/PPO_hungry_geese/test/test_env.py
+++ examples/PPO_hungry_geese/test/test_env.py
@@ -1,7 +1,7 @@
 '''
 Author: hanyu
 Date: 2021-06-09 09:23:32
-LastEditTime: 2021-06-15 10:14:59
+LastEditTime: 2021-06-16 09:08:09
 LastEditors: hanyu
 Description: test env
 FilePath: /test_ppo/examples/PPO_hungry_geese/test/test_env.py
@@ -16,5 +16,8 @@ env = Env(debug=True)
 obs = env.reset(True)
 is_terminal = False
 while not is_terminal:
-    actions = {idx: random.choice([0, 1, 2, 3]) for idx in range(env.num_agents)}
-    obs, reward, is_terminal, info = env.step(actions)
+    actions = {idx: random.choice([0, 1, 2, 3])
+               for idx in range(env.num_agents)}
+    obs, reward, is_terminal, info = env.step(actions,
+                                              [0] * env.num_agents,
+                                              [0] * env.num_agents)
