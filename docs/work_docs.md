eb0ec1e806c16455d88e9171ed124334c21004f7
张 帆 <zhangfan06@mail.jj.cn>
2021-07-09 10:30:15 +0000

    Update "ppo alogrithm picture"

--- docs/work_docs.md
+++ docs/work_docs.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: hanyu
  * @Date: 2021-06-15 10:34:08
- * @LastEditTime: 2021-06-29 02:05:50
+ * @LastEditTime: 2021-07-09 10:29:55
  * @LastEditors: hanyu
  * @Description: work docs
  * @FilePath: /RL_Lab/docs/work_docs.md
@@ -38,6 +38,28 @@
 1. Start training rllib-model, command: `python examples/PPO_hungry_geese/rllib_ppo/train.py --exper_name rllib-hungrygeese-agent_4-gamma_0.99-reuse_3 --ray_workers 15 --rollout_fragment_length 2000 --gamma 0.99 --num_gpus 1 --train_batch_size 30000 --checkpoint_freq 200 --num_sgd_iter 3 --num_policies 4`
 2. Decorator for getting&setting Tensorflow models' weights
 3. Two utils functions: `get_tensor_shape`, `draw_categorical_samples`
+#### 2. Todo
+1. PPO algorithm based on Ray distribution cluster.
+2. Hungery-Geese Environment using ray-ppo.
+3. Finish Actor-Net and Critic-Net
+4. policy_model.py warp_model()
+
+### 2021-07-08
+#### 1. Done
+1. Submit the submission.py to kaggle and fix a series problem, scores: 600
+2. Continue training rllib mutil-agent hungry-geese, `PYTHONPATH=./ python examples/PPO_hungry_geese/rllib_ppo/train.py --exper_name rllib-hungrygeese-agent_4-gamma_0.99-reuse_1 --ray_workers 15 --rollout_fragment_length 2000 --gamma 0.99 --num_gpus 1 --train_batch_size 30000 --checkpoint_freq 200 --num_sgd_iter 1 --num_policies 4 --model_restore /home/jj/workspace/hanyu/RL_Lab/result/hungry_geese/rllib-hungrygeese-agent_4-gamma_0.99-reuse_3/PPO_HungryGeeseEnv_1c317_00000_0_2021-06-30_07-09-28/checkpoint_002000/checkpoint-2000`
+
+#### 2. Todo
+1. PPO algorithm based on Ray distribution cluster.
+2. Hungery-Geese Environment using ray-ppo.
+3. Finish Actor-Net and Critic-Net
+4. policy_model.py warp_model()
+
+### 2021-07-09
+#### 1. Done
+1. PPO alogrithm picture
+2. actor net & critic net
+
 #### 2. Todo
 1. PPO algorithm based on Ray distribution cluster.
 2. Hungery-Geese Environment using ray-ppo.
--- examples/PPO_hungry_geese/ray_ppo/policy_model.py
+++ examples/PPO_hungry_geese/ray_ppo/policy_model.py
@@ -1,7 +1,7 @@
 '''
 Author: hanyu
 Date: 2021-06-22 12:46:08
-LastEditTime: 2021-06-30 12:10:32
+LastEditTime: 2021-07-09 09:01:59
 LastEditors: hanyu
 Description: policy network of PPO
 FilePath: /RL_Lab/examples/PPO_hungry_geese/ray_ppo/policy_model.py
@@ -15,6 +15,7 @@ def wrap_model():
     """warpper of policy model
     """
     import tensorflow as tf
+    from utils.get_tensor_shape import get_tensor_shape
 
     @tf_model_ws
     class PolicyModel(object):
@@ -30,18 +31,21 @@ def wrap_model():
             # TODO feature net
 
             # Actor Network
-            self.current_action_logits = self.actor_net(self.s_t, scope + '_current_actor')
+            self.current_action_logits = self.actor_net(
+                self.s_t, scope + '_current_actor')
             # random sample according to logits
             # output shape: [N]
-            self.current_action = tf.squeeze(draw_categorical_samples(self.current_action_logits), axis=-1)
+            self.current_action = tf.squeeze(
+                draw_categorical_samples(self.current_action_logits), axis=-1)
 
             # Critic Network
             self.current_value = tf.critic_net(self.s_t, '_current_critic')
 
             advantage = kwargs.get('adv', None)
-            # TODO
-
-
+            if advantage is not None:
+                # calculate the mean advantage
+                self.old_current_value = kwargs.get('v_cur')
+                self.ret = advantage + self.old_current_value
 
         def feature_net(self, s, prev_a, prev_r, scope='feature') -> tuple:
             """feature extract network
@@ -64,11 +68,36 @@ def wrap_model():
             Args:
                 s (Tensor): state tensor
                 scope (str): name scope
-            """            
+            """
             input = s
             with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
-                input = tf.layers.dense(input, )
-        
-        def critic_net():
-            # TODO
-            pass
+                input = tf.layers.dense(input,
+                                        get_tensor_shape(s)[-1],
+                                        tf.nn.rule,
+                                        name='dense')
+                act_logits = tf.layers.dense(
+                    input,
+                    self.act_space,
+                    activation=None,
+                    name='a_logits')
+            return act_logits
+
+        def critic_net(self, s, scope):
+            """critic network
+
+            Args:
+                s (Tensor): state tensor
+                scope (str): name scope
+            """
+            input = s
+            with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+                input = tf.layers.dense(input,
+                                        get_tensor_shape(s)[-1],
+                                        tf.nn.rule,
+                                        name='dense')
+                v_value = tf.layers.dense(
+                    input,
+                    self.act_space,
+                    activation=None,
+                    name='v_value')
+            return v_value
