f54f0bc00564ec382cf0c5fda9d1e2e4f70c7fe8
hybug <hanyu.bug@qq.com>
2021-09-10 03:55:18 +0000

    Update "Hungery-Geese ranks 666 & Begin new competition LuxAI"

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 '''
 Author: hanyu
-Date: 2021-03-30 12:22:56
-LastEditTime: 2021-03-30 12:24:50
+Date: 2021-07-13 10:08:40
+LastEditTime: 2021-07-13 10:20:38
 LastEditors: hanyu
-Description: config file
-FilePath: /MahjongWuHan-SL/config.py
+Description: 
+FilePath: /RL_Lab/config.py
 '''
 import os
 import logging
@@ -13,8 +13,5 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s [%(process)d] [%(filename)s:%(lineno)d] [%(levelname)s]: %(message)s",
-                    datefmt="[%Y-%m-%dT%H:%M:%S]",
-                    # filename=BASEDIR + '/logs/log.txt',
-                    filemode='a'
-                    )
+                    datefmt="[%Y-%m-%dT%H:%M:%S]")
 logger = logging.getLogger(__name__)
--- examples/PPO_hungry_geese/ray_ppo-bak/policy_model.py
+++ examples/PPO_hungry_geese/ray_ppo-bak/policy_model.py
@@ -1,7 +1,7 @@
 '''
 Author: hanyu
 Date: 2021-06-22 12:46:08
-LastEditTime: 2021-07-09 09:01:59
+LastEditTime: 2021-07-13 09:27:41
 LastEditors: hanyu
 Description: policy network of PPO
 FilePath: /RL_Lab/examples/PPO_hungry_geese/ray_ppo/policy_model.py
@@ -29,6 +29,7 @@ def wrap_model():
             self.prev_r = kwargs.get('r')
 
             # TODO feature net
+            
 
             # Actor Network
             self.current_action_logits = self.actor_net(
@@ -43,9 +44,28 @@ def wrap_model():
 
             advantage = kwargs.get('adv', None)
             if advantage is not None:
-                # calculate the mean advantage
+                # normalize the advantage
                 self.old_current_value = kwargs.get('v_cur')
                 self.ret = advantage + self.old_current_value
+
+                self.a_t = kwargs.get('a')
+                self.behavior_logits = kwargs.get('a_logits')
+                self.r_t = kwargs.get('r')
+
+                self.adv_mean = tf.reduce_mean(advantage, axis=[0, 1])
+                advantage -= self.adv_mean
+                self.adv_std = tf.math.sqrt(
+                    tf.reduce_mean(advantage ** 2, axis=[0, 1]))
+                self.advantage = advantage / tf.maximum(self.adv_std, 1e-12)
+
+                self.slots = tf.cast(kwargs.get('slots'), tf.float32)
+
+                # use reward prediction
+                # TODO
+                # use pixel reconstruction
+                # TODO
+                # use piexel control
+                # TODO
 
         def feature_net(self, s, prev_a, prev_r, scope='feature') -> tuple:
             """feature extract network
@@ -59,8 +79,34 @@ def wrap_model():
             Returns:
                 tuple: extracted feature
             """
-            # TODO
-            pass
+            shape = get_tensor_shape(s)
+            with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+                input = tf.reshape(s, [-1] + shape[-3:])
+                filter = [16, 32, 32]
+                kernel = [(3, 3), (3, 3), (5, 3)]
+                stride = [(1, 2), (1, 2), (2, 1)]
+                for i in range(len(filter)):
+                    input = tf.layers.conv2d(
+                        input,
+                        filters=filter[i],
+                        kernel_size=kernel[i][0],
+                        strides=stride[i][0],
+                        padding='valid',
+                        activation=None,
+                        name=f'conv_{i}'
+                    )
+                    input = tf.layers.max_pooling2d(
+                        input,
+                        pool_size=kernel[i][1],
+                        strides=stride[i][1],
+                        padding='valid',
+                        name=f'maxpool_{i}'
+                    )
+                    input = self.residual_block()
+                input = tf.nn.relu(input)
+
+                new_shape = get_tensor_shape(input)
+                image_feature = tf.reshape()
 
         def actor_net(self, s, scope):
             """actor network
@@ -101,3 +147,32 @@ def wrap_model():
                     activation=None,
                     name='v_value')
             return v_value
+
+        @staticmethod
+        def residual_block(input, scope):
+            shape = get_tensor_shape(input)
+            with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+                output = tf.nn.relu(input)
+                output = tf.layers.conov2d(
+                    output,
+                    filters=shape[-1],
+                    kernel_size=3,
+                    strides=1,
+                    padding='same',
+                    activation=None,
+                    name='conv0'
+                )
+                output = tf.nn.relu(output)
+                output = tf.layers.conv2d(
+                    output,
+                    filters=shape[-1],
+                    kernel_size=3,
+                    strides=1,
+                    padding='same',
+                    activation=None,
+                    name='conv1'
+                )
+                output = output + input
+            return output
+        
+    return PolicyModel
